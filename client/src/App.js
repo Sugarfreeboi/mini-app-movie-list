1806076d2f7ed2c2dec4f6bc8b6328ead68057f8
Sugarfreeboi <laurence.portraitart@gmail.com>
2023-07-17 15:19:32 -0700

    Level 2 complete

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,8 @@ import { useEffect, useState } from 'react';
 // ]
 
 function App() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [searchMovie, setSearchMovie] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:8000/movies')
@@ -20,11 +21,26 @@ function App() {
       .catch(err => console.log(`Fetch failed. Error: ${err}`))
   }, []);
 
+  const handleSearchMovie = (event) => {
+    setSearchMovie(event.target.value);
+  }
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchMovie.toLowerCase())
+  );
+
   return (
     <div>
       <h1>Movie List</h1>
+      {/* Search bar */}
+      <input
+        type="text"
+        placeholder="Search for a movie."
+        value={searchMovie}
+        onChange={handleSearchMovie}
+      />
       <ul>
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <li key={movie.id}>
             {movie.title}
           </li>
